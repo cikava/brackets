2a60b308211f9ffe598c073a9d15851a0204629e
cikava <hembitski@outlook.com>
2022-08-12 20:41:15 +0300

    init: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let sl = str.length;
+  let trueorfalse = true;
+  do {
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      let tof = true;
+      let strlength = str.length;
+      do {
+        str=str.replace(bracketsConfig[i][0]+bracketsConfig[i][1],'');
+        if ((str.length == strlength) || (str.length == 0)) tof = false;
+        strlength = str.length;
+      } while (tof);
+    }
+    if ((str.length == sl) || (str.length == 0)) trueorfalse = false;
+    sl = str.length;
+  } while (trueorfalse);
+  return (str == '');
 }
